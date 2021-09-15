aec50b02d07db0d470469332cfff8c284334f035
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-14 19:16:25 -0700

    Working on getting the event listener to display the math function

--- scripts.js
+++ scripts.js
@@ -6,8 +6,11 @@ function secondChange(){
     document.getElementById("testMeow").innerHTML = "2nd Change Now in File";
 }
 
-function math(){
-    let addition = 7 + 3;
-    return addition;
-}
+//function math(){
+//    let addition = 7 + 3;
+//    return addition;
+//}
 
+function displayMath(){
+    document.addEventListener("onload", )
+}
