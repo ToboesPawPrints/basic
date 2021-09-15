48f1695ff0b3a3fd37dd38d9a5b6123cf092e58f
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-14 18:52:37 -0700

    Changed math function to return the addition variable.  Working on getting it to appear in the html.

--- scripts.js
+++ scripts.js
@@ -6,10 +6,8 @@ function secondChange(){
     document.getElementById("testMeow").innerHTML = "2nd Change Now in File";
 }
 
-//function math(){
-//    document.getElementById("mathTest").innerHTML = 7 + 3;
-//}
+function math(){
+    let addition = 7 + 3;
+    return addition;
+}
 
-
-let addition = 7;
-document.write(addition);
